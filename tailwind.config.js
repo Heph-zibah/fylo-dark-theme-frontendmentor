a5ef00aead5040d0ccdc5fbf3fbce2cf90b3a60a
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-09-10 13:40:24 +0100

    update file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    fontFamily: {
+      publicSans: ['Public Sans', 'sans-serif'],
+      openSans: ['Open Sans', 'san-serif'],
+      raleway: ['Raleway', 'sans-serif']
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        white:' hsl(0, 0%, 100%)',
+        darkBlueIntro : 'hsl(217, 28%, 15%)',
+        darkBlueMain: 'hsl(218, 28%, 13%)',
+        darkBlueFooter: 'hsl(216, 53%, 9%)',
+        darkBlueTestimonials:' hsl(219, 30%, 18%)',
+         cyan: 'hsl(176, 68%, 64%)',
+        blue: 'hsl(198, 60%, 50%)',
+        lightRedError: 'hsl(0, 100%, 63%)'
+      }
+    },
   },
   plugins: [],
 }
